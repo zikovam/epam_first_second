4e2c07c618efb53ba45dec22f1df4898e98ebe32
elefus <elefus@yandex.ru>
2016-03-06 02:11:45 +0300

    Create common branch and add interface fisrt task.

---
+++ src/main/java/ru/spb/epam/common/first/ISolver.java
@@ -0,0 +1,144 @@
+package ru.spb.epam.common.first;
+
+/**
+ * Интерфейс первого задания.
+ * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/%D0%97%D0%B0%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5%20%E2%84%961">Описание задания</a>}
+ */
+public interface ISolver {
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.1">Задание №1</a>}
+     */
+    void task1();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.2">Задание №2</a>}
+     */
+    void task2();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.3">Задание №3</a>}
+     */
+    void task3();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.4">Задание №4</a>}
+     */
+    void task4();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.5">Задание №5</a>}
+     */
+    void task5();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.6">Задание №6</a>}
+     */
+    void task6();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.7">Задание №7</a>}
+     */
+    void task7();
+
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.8">Задание №8</a>}
+     */
+    void task8();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.9">Задание №9</a>}
+     */
+    void task9();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.10">Задание №10</a>}
+     */
+    void task10();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.11">Задание №11</a>}
+     */
+    void task11();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.12">Задание №12</a>}
+     */
+    void task12();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.13">Задание №13</a>}
+     */
+    void task13();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.14">Задание №14</a>}
+     */
+    void task14();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.15">Задание №15</a>}
+     */
+    void task15();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.16">Задание №16</a>}
+     */
+    void task16();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.17">Задание №17</a>}
+     */
+    void task17();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.18">Задание №18</a>}
+     */
+    void task18();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.19">Задание №19</a>}
+     */
+    void task19();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.20">Задание №20</a>}
+     */
+    void task20();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.21">Задание №21</a>}
+     */
+    void task21();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.22">Задание №22</a>}
+     */
+    void task22();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.23">Задание №23</a>}
+     */
+    void task23();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.24">Задание №24</a>}
+     */
+    void task24();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.25">Задание №25</a>}
+     */
+    void task25();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.26">Задание №26</a>}
+     */
+    void task26();
+
+    /**
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-2016/wiki/Task%201.27">Задание №27</a>}
+     */
+    void task27();
+}
