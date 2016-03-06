afe02b4a38a469132dce7b2407c2dfd4dcb5e40e
elefus <elefus@yandex.ru>
2016-03-07 01:27:10 +0300

    Add implementation ISolver

---
+++ src/main/java/ru/spb/epam/azykov/first/Solver.java
@@ -0,0 +1,143 @@
+package ru.spb.epam.azykov.first;
+
+import ru.spb.epam.common.first.ISolver;
+
+/**
+ * Реализует выполнение первого задания
+ */
+public class Solver implements ISolver {
+
+	@Override
+    public void task1() {
+		
+    }
+
+	@Override
+    public void task2() {
+		
+    }
+
+    public void task3() {
+
+    }
+
+    @Override
+    public void task4() {
+
+    }
+
+    @Override
+    public void task5() {
+		
+    }
+
+    @Override
+    public void task6() {
+
+    }
+
+    @Override
+    public void task7() {
+
+    }
+
+    @Override
+    public void task8() {
+
+    }
+
+    @Override
+    public void task9() {
+
+    }
+
+    @Override
+    public void task10() {
+
+    }
+
+    @Override
+    public void task11() {
+
+    }
+
+    @Override
+    public void task12() {
+
+    }
+
+    @Override
+    public void task13() {
+
+    }
+
+    @Override
+    public void task14() {
+
+    }
+
+    @Override
+    public void task15() {
+
+    }
+
+    @Override
+    public void task16() {
+
+    }
+
+    @Override
+    public void task17() {
+
+    }
+
+    @Override
+    public void task18() {
+
+    }
+
+    @Override
+    public void task19() {
+
+    }
+
+    @Override
+    public void task20() {
+
+    }
+
+    @Override
+    public void task21() {
+
+    }
+
+    @Override
+    public void task22() {
+
+    }
+
+    @Override
+    public void task23() {
+
+    }
+
+    @Override
+    public void task24() {
+
+    }
+
+    @Override
+    public void task25() {
+
+    }
+
+    @Override
+    public void task26() {
+
+    }
+
+    @Override
+    public void task27() {
+
+    }
+}
