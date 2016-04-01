c065fd65426ee67ef5c17c3848e43dfa06684d5f
Alexandr Zykov <zikovam43@gmail.com>
2016-04-01 15:46:23 +0300

    azykov 19 commit
    THAT'S ALL №2!

--- src/main/java/ru/spb/epam/azykov/first/Solver.java
+++ src/main/java/ru/spb/epam/azykov/first/Solver.java
@@ -663,7 +663,6 @@ public class Solver implements ISolver {
             System.out.println (matrix.size ());
             System.out.println (matrix.get (0).size ());
         }
-
         for (int i = 0; i < matrix.size (); i++) {
             for (int j = 0; j < matrix.get (0).size (); j++) {
                 System.out.print (matrix.get (i).get (j)+"\t");
